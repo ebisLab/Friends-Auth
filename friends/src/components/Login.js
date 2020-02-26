a547072a9fd856de8891a05635bd6a00b7a054cf
ebisLab <ebaiden@techie.com>
2020-02-26 09:23:45 -0500

    login successful

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,46 +1,49 @@
 import React,{useState} from 'react';
-
-
+import axios from 'axios'
 const Login = () =>{
     const [inputData, setInputData] = useState({
-        name: '', 
-        password: ''
-    })
-    const [data, setData] = useState("")
+          username: '', 
+        password: ''  
 
+    })
+    const [data, setData] = useState([])
     const getLoggedIn = (peep)=>{
 const newLog = {
     id: Date.now(), 
-    name: peep.name,
+    name: peep.username,
     password: peep.password
 }
 setData([...data, newLog])
     }
-
     const changeHandler=e=>{
-
 setInputData({...inputData, [e.target.name]: e.target.value})
+console.log(inputData, 'input data creds')
 console.log(e.target.value)
     }
-
-    const submitHandler = e =>{
-        e.preventDefault();
-        getLoggedIn(inputData);
-        console.log('Submit clicked', inputData)
-    }
-
+    // const submitHandler = e =>{
+    //     e.preventDefault();
+    //     getLoggedIn(inputData);
+    //     console.log('Submit clicked', inputData)
+    // }
+    const login = e =>{
+        e.preventDefault()
+        axios
+        .post(`http://localhost:5000/api/login`, inputData )
+        .then(res =>
+            console.log(res))
+        .catch(err => err )
+        }
     return (
         <div>
             <h1>E.B.'s Friends App (Form)</h1>
-<form onSubmit={submitHandler}>
+<form onSubmit={login}>
     <input 
     type="text" 
-    label="name"
-    name="name" 
-    value={inputData.name} 
-    placeholder="name" 
+    label="username"
+    name="username" 
+    value={inputData.username} 
+    placeholder="username" 
     onChange={changeHandler} />
-
 <input 
     type="password" 
     label="password"
@@ -48,13 +51,9 @@ console.log(e.target.value)
     value={inputData.password} 
     placeholder="password" 
     onChange={changeHandler} />
-
         <button>Login</button>
 </form>
-        
-
         </div>
     )
 }
-
 export default Login;
