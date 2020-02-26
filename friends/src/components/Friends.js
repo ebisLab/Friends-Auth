52e8d117ef24a2f7a07d388cd7e817cd9fb9dc21
ebisLab <ebaiden@techie.com>
2020-02-26 10:26:11 -0500

    get axios call

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios'
 
 
 const Friends = () =>{
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+       axios
+       .get(`http://localhost:5000/api/friends`)
+       .then(res => 
+        console.log('res', res))
+        .catch(err =>
+            console.log(err))
+    }, [])
 
     return (
         <div>
